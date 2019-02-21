2b8a2d3a35965df11e62c2336baea3acb9899487
Mohammad <moh.rangwala.29@gmail.com>
2019-02-21 18:10:59 +0530

    Android Splash Screen Done

--- ios/UrbanClapDemo/AppDelegate.m
+++ ios/UrbanClapDemo/AppDelegate.m
@@ -29,6 +29,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen showSplash:@"LaunchScreen" inRootView:rootView];
   return YES;
 }
 
---
+++ src/MainApp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class MainApp extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    heloo
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default MainApp;
